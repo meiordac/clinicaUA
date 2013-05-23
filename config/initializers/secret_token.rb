439ac028b88b563e37fd497b447e4152b95244c9
Matias Iordache <sindarghost@gmail.com>
2013-05-22 20:48:26 -0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ClinicaUA::Application.config.secret_token = 'ec2203b37ca4a6fa3b7c810c7fb832388a9c77cacaa23a3fbe46b4d676a22753b4e1916bf1afbcebfb178eb931a065b5749ff84de4f6b3ebbc693fa34ea5ed96'
